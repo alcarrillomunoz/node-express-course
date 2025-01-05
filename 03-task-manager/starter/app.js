682718542020a2c9129d965776069c4d8715bd37
Lily <alcarrillomunoz@gmail.com>
2025-01-05 17:58:18 -0500

    week 6 assignment: Task Manager API - part 2

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,26 +3,33 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes 
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App')
-});
+// app.get('/hello', (req, res) => {
+//     res.send('Task Manager App')
+// });
 
 app.use('/api/v1/tasks', tasks);
 
+//error handeling middleware 
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 // port set up
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening on port ${port}...`));
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
 
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,29 +1,63 @@
 const Task = require('../models/Task');     //schema 
+const asyncWrapper = require('../middleware/async');
+const { createCustomError } = require('../errors/custom-error')
 
 // get all tasks 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
-}
+const getAllTasks = asyncWrapper (async (req, res) => {
+    const tasks = await Task.find({})   //passing in empty object will return all items in the collection
+    res.status(200).json({ tasks, totalTasks:tasks.length })
+})
 
 // create new task 
-const createTask = async (req, res) => {
+const createTask = asyncWrapper (async (req, res) => {
     const task = await Task.create(req.body)
     res.status(201).json({ task })
-}
+})
 
 // get one task 
-const getTask = (req, res) => {
-    res.json({id:req.params.id})
-}
+const getTask = asyncWrapper(async (req, res, next) => {
+    const {id:taskID} = req.params
+    const task = await Task.findOne({ _id:taskID })
+    if (!task) {
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
+    }
+    res.status(200).json({ task })
+})
 
 // update task 
-const updateTask = (req, res) => {
-    res.send('update task')
-}
+const updateTask = asyncWrapper(async (req, res) => {
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {new:true, runValidators:true})
+    if (!task) {
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
+    }
+    res.status(200).json({id:taskID, data:req.body})
+})
 
 // delete task 
-const deleteTask = (req, res) => {
-    res.send('delete task')
-}
+const deleteTask = asyncWrapper(async (req, res) => {
+    const {id:taskID} = req.params;
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if (!task) {
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
+    }
+    res.status(200).json({ task })
+})
+
+// (put method will replace existing item, patch will update only properties passed in) 
+// edit task
+// const editTask = async (req, res) => {
+//     try {
+//         const {id:taskID} = req.params;
+//         const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {new:true, runValidators:true, overwrite:true})
+//         if (!task) {
+//             return res.status(404).json({ msg: `No task with id: ${taskID}`})
+//         }
+//         res.status(200).json({ task })
+//     }
+//     catch (error) {
+//         res.status(500).json({ msg:error })
+//     }
+// }
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask }
